2f0b809fb06fddfc962d661f9e57b800fd0b3101
i-seo725 <hjing719@gmail.com>
2023-11-20 11:46:24 +0900

    Initial Commit

---
+++ CoinOrderBook/CoinOrderBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinOrderBookApp.swift
+//  CoinOrderBook
+//
+//  Created by 이은서 on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinOrderBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
